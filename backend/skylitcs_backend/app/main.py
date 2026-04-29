6406f87aa9d71da16d45b846f480b029aa333dd1
Abuzarr1 <abuzarr819@gmail.com>
2026-04-30 02:48:38 +0500

    Force allow all CORS origins for testing

--- backend/skylitcs_backend/app/main.py
+++ backend/skylitcs_backend/app/main.py
@@ -10,14 +10,13 @@ def create_app() -> FastAPI:
         openapi_url=f"{settings.API_V1_STR}/openapi.json"
     )
 
-    if settings.BACKEND_CORS_ORIGINS:
-        app.add_middleware(
-            CORSMiddleware,
-            allow_origins=["*"],
-            allow_credentials=True,
-            allow_methods=["*"],
-            allow_headers=["*"],
-        )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     @app.get("/health", tags=["system"])
     async def health_check():
